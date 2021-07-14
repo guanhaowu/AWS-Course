a67cf6a35c7ac113090de542d786b075c36e12dc
Guan Hao Wu <guanhao_wu@hotmail.com>
2021-07-14 02:05:44 +0200

    Copy of CodeCommit repo Album-Api
    Lecture 3 Assignment - Copy from CodeCommit repo Album-Api.

---
+++ lecture_3/Album-Api/Album.Api.Tests/IntegrationTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Album.Api.Tests
+{
+    public class IntegrationTest : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        public HttpClient Client { get; }
+
+        public IntegrationTest(WebApplicationFactory<Startup> fixture)
+        {
+            Client = fixture.CreateClient();
+        }
+
+
+        [Theory]
+        [InlineData("api/Hello")]
+        public async Task TestEndPoints(string endpoint)
+        {
+            var response = await Client.GetAsync(endpoint);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
---
+++ lecture_3/Album-Api/Album.Api/Models/Hello.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Album.Api.Models
+{
+  public class Hello
+  {
+    public string Name { get; set; }
+  }
+}
